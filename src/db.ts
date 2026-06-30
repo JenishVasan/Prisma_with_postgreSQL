277f579b45d8d3b506bc8bba4ba84cd93eba8ba8
JenishVasan <jenishvasan3@gmail.com>
2026-06-30 14:56:08 +0530

    sync package.json, db.ts, and index.js to working versions

--- src/db.ts
+++ src/db.ts
@@ -1,19 +1,14 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/prisma/client.js";
-import pg from "pg";
+import { PrismaClient } from "./generated/prisma/client.ts";
 
 const globalForPrisma = globalThis as unknown as {
     prisma?: PrismaClient;
 };
 
-// Initialize the pg Pool and the Prisma adapter
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
-
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 export const prisma =
     globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
     globalForPrisma.prisma = prisma;
 }
-
